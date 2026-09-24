835f46ec41ad8e333780020c9b6684dc966b1dc2
Angela <100570473@alumnos.uc3m.es>
2026-09-24 19:10:50 +0200

    isrf

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -1,12 +1,11 @@
-
 # MAIN FUNCTION TO CALL THE ISM MODULE
 
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
+auxdir = r"C:\Users\jesus\Desktop\MISE\5SEMICUATRIMESTRE\EODP\EODC_Code_2\auxiliary"
+indir = r"C:\Users\jesus\Desktop\MISE\5SEMICUATRIMESTRE\EODP\EODP_TER_2021-20260917T101917Z-1-001\EODP_TER_2021\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
+outdir = r"C:\Users\jesus\Desktop\MISE\5SEMICUATRIMESTRE\EODP\EODP_TER_2021-20260917T101917Z-1-001\EODP_TER_2021\EODP-TS-ISM\output"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
